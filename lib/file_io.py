a0c120b776bba61119d72df63f210c583495380c
Adonis Pierre <bigirimanaadonis@gmail.com>
2025-11-27 15:52:41 +0300

    lab24

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
+# lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    """Writes content to a file. Overwrites if file exists."""
+    with open(f"{file_name}.txt", "w") as f:
+        f.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    """Appends content to a file."""
+    with open(f"{file_name}.txt", "a") as f:
+        f.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    """Reads the content of a file and returns it."""
+    with open(f"{file_name}.txt", "r") as f:
+        return f.read()
